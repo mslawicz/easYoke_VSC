104f466b2f0004868e53e7f580121433a913e2f8
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-09-28 10:22:00 +0200

    characteristic report map with 128-bit UUID

--- STM32_WPAN/App/custom_stm.c
+++ STM32_WPAN/App/custom_stm.c
@@ -95,6 +95,16 @@ static tBleStatus Generic_STM_App_Update_Char_Ext(uint16_t ConnectionHandle, uin
 
 /* Private functions ----------------------------------------------------------*/
 
+#define COPY_UUID_128(uuid_struct, uuid_15, uuid_14, uuid_13, uuid_12, uuid_11, uuid_10, uuid_9, uuid_8, uuid_7, uuid_6, uuid_5, uuid_4, uuid_3, uuid_2, uuid_1, uuid_0) \
+do {\
+    uuid_struct[0] = uuid_0; uuid_struct[1] = uuid_1; uuid_struct[2] = uuid_2; uuid_struct[3] = uuid_3; \
+    uuid_struct[4] = uuid_4; uuid_struct[5] = uuid_5; uuid_struct[6] = uuid_6; uuid_struct[7] = uuid_7; \
+    uuid_struct[8] = uuid_8; uuid_struct[9] = uuid_9; uuid_struct[10] = uuid_10; uuid_struct[11] = uuid_11; \
+    uuid_struct[12] = uuid_12; uuid_struct[13] = uuid_13; uuid_struct[14] = uuid_14; uuid_struct[15] = uuid_15; \
+}while(0)
+
+#define COPY_REPORTMAP_UUID(uuid_struct)    COPY_UUID_128(uuid_struct,0x00,0x00,0x2a,0x4b,0x8e,0x22,0x45,0x41,0x9d,0x4c,0x21,0xed,0xae,0x82,0xed,0x19)
+
 /* USER CODE BEGIN PF */
 
 /* USER CODE END PF */
@@ -266,9 +276,9 @@ void SVCCTL_InitCustomSvc(void)
   /**
    *  ReportMap
    */
-  uuid.Char_UUID_16 = 0x0000;
+  COPY_REPORTMAP_UUID(uuid.Char_UUID_128);
   ret = aci_gatt_add_char(CustomContext.CustomHidHdle,
-                          UUID_TYPE_16, &uuid,
+                          UUID_TYPE_128, &uuid,
                           SizeRep_Map,
                           CHAR_PROP_READ | CHAR_PROP_WRITE_WITHOUT_RESP,
                           ATTR_PERMISSION_NONE,
